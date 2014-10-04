b7648b5949e266aa00b33c3b8946fd61a2c0c5cd
Frode Bjerke <frode.bjerke@gmail.com>
2014-10-04 12:29:27 +0200

    added mithril - webapp hello world

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log("hello");
+var m = require('mithril');
+
+m.route(document.getElementById("main-region"), "/", {
+  "/": {
+    controller: function () {
+
+    },
+    view: function (ctrl) {
+      return m("", "Halla mithril");
+    }
+  }
+});
